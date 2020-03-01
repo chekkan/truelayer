12d1355b2ef712269a97c4db5f053344bcf281bd
Harish Babu <harish_babu@outlook.com>
2020-03-01 13:03:12 +0000

    move ProvidersController to Api.Controllers namespace

--- src/Api/Controllers/ProvidersController.cs
+++ src/Api/Controllers/ProvidersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 
-namespace Api.TrueLayer.Controllers
+namespace Api.Controllers
 {
     [Route("v1/providers")]
     [Controller]
